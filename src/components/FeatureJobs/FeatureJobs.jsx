00043717a0565bcb0fcb8155292a80d841d34773
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2023-04-12 19:04:59 +0600

    See all jobs button addded

--- src/components/FeatureJobs/FeatureJobs.jsx
+++ src/components/FeatureJobs/FeatureJobs.jsx
@@ -1,16 +1,40 @@
 import React, { useEffect, useState } from "react";
 import FeatureJob from "../FeatureJob/FeatureJob";
+import "./FeatureJobs.css"
 
 const FeatureJobs = () => {
   const [features, setFeatures] = useState([]);
+  const [seeAll ,setSeeAll] = useState(false)
+  let values;
   useEffect(() => {
     fetch("feature-job.json")
       .then((res) => res.json())
-      .then((data) => setFeatures(data));
-  }, []);
+      // .then((data) => setFeatures(data));
+      .then((data) => {
+        values = data;
+        let dataLimit = values.slice(0, 4);
+
+        seeAll ? setFeatures(data) : setFeatures(dataLimit)
+        // setFeatures(dataLimit)
+        seeAllJobs(data,dataLimit)
+        // setFeatures(dataLimit);
+      });
+  }, [seeAll]);
+
+
+
+  
+  const seeAllJobs = (data) => {
+
+    console.log('data');
+    // setFeatures(data)  // automaticly called why??
+    // setFeatures(values);
+  };
 
   return (
-    <div className="flex flex-col justify-center my-32 mx-48 ">
+    <div>
+      {/* {seAll ? setFeatures(): ""} */}
+      <div className="flex flex-col justify-center my-32 mx-48 ">
       <h1 className="text-4xl text-center font-bold my-3">Featured Jobs</h1>
       <p className="text-center text-slate-500 mb-7">
         Explore thousands of job opportunities with all the information you
@@ -21,6 +45,12 @@ const FeatureJobs = () => {
           <FeatureJob key={feature.id} feature={feature} />
         ))}
       </div>
+      <div className="flex justify-center">
+      <button onClick={() => setSeeAll(!seeAll)} className="text-center see-all-jobs">
+        See all Jobs
+      </button>
+      </div>
+    </div>
     </div>
   );
 };
--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLoaderData, useParams, useSearchParams } from "react-router-dom";
-import "./JobDetails.css"
+import "./JobDetails.css";
 import { addToDB } from "../utilities/addToDB";
 
 const JobDetails = () => {
@@ -27,8 +27,10 @@ const JobDetails = () => {
   } = searchInfo;
 
   return (
-    <div className="mx-52">
-      <h3 className="text-4xl font-semibold text-center mt-24 mb-64 mr-5">Job Details</h3>
+    <div className="mx-52 mb-32">
+      <h3 className="text-4xl font-semibold text-center mt-24 mb-64 mr-5">
+        Job Details
+      </h3>
       <div className="flex sm:flex-col md:flex-row lg:flex-row justify-center">
         <div className=" w-9/12 mb-12">
           <p>
@@ -57,21 +59,41 @@ const JobDetails = () => {
         <div>
           <div className="box rounded-md mr-8">
             <h5 className="font-bold p-3 pl-6">Job Details</h5>
-            <hr className="mx-4 font-extrabold"/>
-            <p className="pl-3"><span className="font-semibold">Salary :</span> <span className="text-slate-600">{salary} (Per Month) </span> </p>
-            <p className="pl-3"><span className="font-semibold">Job Title :</span> <span className="text-slate-600">{jobTitle}</span></p>
+            <hr className="mx-4 font-extrabold" />
+            <p className="pl-3">
+              <span className="font-semibold">Salary :</span>{" "}
+              <span className="text-slate-600">{salary} (Per Month) </span>{" "}
+            </p>
+            <p className="pl-3">
+              <span className="font-semibold">Job Title :</span>{" "}
+              <span className="text-slate-600">{jobTitle}</span>
+            </p>
 
             <div className="p-7">
               <h5 className="font-semibold break-all border-b-2 p-2">
                 Contact Information
               </h5>
-              <p><span className="font-semibold">Phone :</span> <span className="text-slate-600">{phone}</span></p>
-              <p><span className="font-semibold">Email :</span> <span className="text-slate-600">{email}</span></p>
-              <p><span className="font-semibold">Address :</span> <span className="text-slate-600">{jobLocation}</span></p>
+              <p>
+                <span className="font-semibold">Phone :</span>{" "}
+                <span className="text-slate-600">{phone}</span>
+              </p>
+              <p>
+                <span className="font-semibold">Email :</span>{" "}
+                <span className="text-slate-600">{email}</span>
+              </p>
+              <p>
+                <span className="font-semibold">Address :</span>{" "}
+                <span className="text-slate-600">{jobLocation}</span>
+              </p>
             </div>
           </div>
           <div className="pr-8">
-          <button onClick={() => addToDB(id)} className="btn-apply-now w-full">Apply Now</button>
+            <button
+              onClick={() => addToDB(id)}
+              className="btn-apply-now w-full"
+            >
+              Apply Now
+            </button>
           </div>
         </div>
       </div>
@@ -79,4 +101,3 @@ const JobDetails = () => {
   );
 };
 export default JobDetails;
- 
